dac4671df8ccc5e34048975ab9d4d33562b3363a
mgsa <maaurin@robotics.utias.utoronto.ca>
2021-03-08 10:54:44 +0100

    Costmap computation ok

---
+++ catkin_ws/build/teb_local_planner/CMakeFiles/teb_local_planner_gencfg.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/teb_local_planner_gencfg"
+  "/home/mgsa/devfile/catkin_ws/devel/include/teb_local_planner/TebLocalPlannerReconfigureConfig.h"
+  "/home/mgsa/devfile/catkin_ws/devel/share/teb_local_planner/docs/TebLocalPlannerReconfigureConfig.dox"
+  "/home/mgsa/devfile/catkin_ws/devel/share/teb_local_planner/docs/TebLocalPlannerReconfigureConfig-usage.dox"
+  "/home/mgsa/devfile/catkin_ws/devel/lib/python2.7/dist-packages/teb_local_planner/cfg/TebLocalPlannerReconfigureConfig.py"
+  "/home/mgsa/devfile/catkin_ws/devel/share/teb_local_planner/docs/TebLocalPlannerReconfigureConfig.wikidoc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/teb_local_planner_gencfg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ catkin_ws/build/teb_local_planner/CMakeFiles/teb_local_planner_generate_messages_lisp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/teb_local_planner_generate_messages_lisp"
+  "/home/mgsa/devfile/catkin_ws/devel/share/common-lisp/ros/teb_local_planner/msg/FeedbackMsg.lisp"
+  "/home/mgsa/devfile/catkin_ws/devel/share/common-lisp/ros/teb_local_planner/msg/TrajectoryPointMsg.lisp"
+  "/home/mgsa/devfile/catkin_ws/devel/share/common-lisp/ros/teb_local_planner/msg/TrajectoryMsg.lisp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/teb_local_planner_generate_messages_lisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
